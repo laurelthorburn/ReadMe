de140f5480cf4dd7800e789619c5f6fc24833d17
Laurel Thorburn <laurelthorburn@gmail.com>
2021-10-14 17:35:36 -0600

    Inital commit

---
+++ index.js
@@ -0,0 +1,40 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+    "What is the project's title?",
+    "Project Description?",
+    "Installation Instructions?",
+    "Usage?",
+    "Contributing?",
+    "Tests?",
+    "License?",
+    "Github Username?",
+    "Email address?"
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
+
+//from inquirer's documentation:
+// var inquirer = require('inquirer');
+// inquirer
+//   .prompt([
+//     /* Pass your questions in here */
+//   ])
+//   .then((answers) => {
+//     // Use user feedback for... whatever!!
+//   })
+//   .catch((error) => {
+//     if (error.isTtyError) {
+//       // Prompt couldn't be rendered in the current environment
+//     } else {
+//       // Something else went wrong
+//     }
+//   });
