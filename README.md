1686ca9fa973c54f55a19ab309bd40ff2906bb76
Laurel Thorburn <laurelthorburn@gmail.com>
2021-10-22 12:33:13 -0600

    Created badge near description per A/C

--- README.md
+++ README.md
@@ -1,25 +1,33 @@
-# gfdgfdgsdfg
-  <a name="desc"></a>
-## Description
-dfgfdgdfg  
+# test
 
-# Table of Contents
+<a name="desc"></a>
+## Description
+test  
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+
+## Table of Contents
 1. [ Description. ](#desc)
-2. [ License. ](#license)
-3. [ Links. ](#links)
-4. [ User Story. ](#story)
-5. [ Acceptance Criteria. ](#acceptance)
-6. [ Website Video. ](#video)
-7. [ Website Screenshots. ](#screenshots)
-8. [ Contributing. ](#contrib)
-9. [ Questions. ](#questions)
-10. [ Resources/Credit. ](#credit)
+2. [ Installation. ](#install)
+3. [ Usage. ](#usage)
+4. [ License. ](#license)
+5. [ Contributing. ](#contrib)
+6. [ Tests. ](#test)
+7. [ Questions. ](#questions)
+
+<a name="install"></a>
+## Installation
+test
+
+<a name="usage"></a>
+## Usage
+test
 
 <a name="license"></a>
-# License
-  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
-
-    Copyright <2021> <undefined>
+## License
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+Copyright <2021>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
@@ -27,48 +35,18 @@ The above copyright notice and this permission notice shall be included in all c
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-<a name="links"></a>
-# Links
-  
-Github: rewrewr
-  
-Deployed Site: dsfzxc
-
-<a name="story"></a>
-# User Story
-  
-  ```
-xzzxc
-  ```
-
-  <a name="acceptance"></a>
-# Acceptance Criteria
-  
-  ```
-rtrwt
-  ```
-  
-  <a name="video"></a>
-  # Website Video
-  
-  ![INSERT IMG SHORT DESCRIPTION]( gif link here (convert mov4/webm to gif: https://cloudconvert.com/webm-to-gif) )
-  
-  <a name="screenshots"></a>
-  # Website Screenshots
-  
   <a name="contrib"></a>
   # Contributing
   
-  * wrtreg
+  * test
+
+  <a name="test"></a>
+  ## Tests
+  test
 
   <a name="questions"></a>
   # Questions?
-  Want to see more of my work? [Click here!](https://github.com/dgdfg)
+  Want to see more of my work? [Click here!](https://github.com/test)
 
-  Questions/comments/concerns? Please send an email to bcvbvcb
-  
-  <a name="credit"></a>
-  # Resources/Credit
-  
-  * LINK HERE
+  Questions/comments/concerns? Please send an email to test
   
--- index.js
+++ index.js
@@ -27,9 +27,19 @@ const promptUser = () => {
         message: "What are the usage instructions?"
     },
     {
+      type: "list",
+      name: "license", 
+      choices: ["MIT License", "IBM Public License Version 1.0", "The Apache License", "Creative Commons - CC0", "Eclipse Public License (EPL)"]
+  },
+    {
         type: "input",
         name: "contributing",
-        message: "Please enter contribution instructions:"
+        message: "Please enter contribution guidelines:"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Enter test instructions:"
     },
     {
       type: "input",
@@ -41,24 +51,12 @@ const promptUser = () => {
         name: "email",
         message: "Please enter collaborator's email address:"
     },
-    {
-        type: "input",
-        name: "tests",
-        message: "Any tests?"
-    },
-    {
-        type: "list",
-        name: "license", 
-        choices: ["MIT License", "IBM Public License Version 1.0", "The Apache License", "Creative Commons - CC0", "Eclipse Public License (EPL)"]
-    },
   ]);
 };
 
-const licenseBadge = (badge, userName) => {
-  if (badge === "MIT License") {
-    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
-
-    Copyright <2021>
+const copyrightText = (copyright) => {
+  if (copyright === "MIT License") {
+    return `Copyright <2021>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
@@ -66,16 +64,13 @@ The above copyright notice and this permission notice shall be included in all c
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`
   }
-  if (badge === "IBM Public License Version 1.0"){
-    return `[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)
-    THE ACCOMPANYING PROGRAM IS PROVIDED UNDER THE TERMS OF THIS IBM PUBLIC LICENSE ("AGREEMENT"). ANY USE, REPRODUCTION OR DISTRIBUTION OF THE PROGRAM CONSTITUTES RECIPIENT'S ACCEPTANCE OF THIS AGREEMENT.
+  if (copyright === "IBM Public License Version 1.0"){
+    return `THE ACCOMPANYING PROGRAM IS PROVIDED UNDER THE TERMS OF THIS IBM PUBLIC LICENSE ("AGREEMENT"). ANY USE, REPRODUCTION OR DISTRIBUTION OF THE PROGRAM CONSTITUTES RECIPIENT'S ACCEPTANCE OF THIS AGREEMENT.
 
     For more information, visit: https://opensource.org/licenses/IPL-1.0`
   }
-  if (badge === "The Apache License"){
-    return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
-
-Copyright 2020 The Apache Software Foundation
+  if (copyright === "The Apache License"){
+    return `Copyright 2020 The Apache Software Foundation
 
 This product includes software developed at
 The Apache Software Foundation (http://www.apache.org/).
@@ -89,27 +84,46 @@ is Adobe Systems Incorporated (http://www.adobe.com/).
 Copyright 2009 - 2013 Adobe Systems Incorporated. All Rights Reserved.
 `
   }
-  if (badge === "Creative Commons - CC0"){
-    return `[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)
-
-    CC0 (aka CC Zero) is a public dedication tool, which allows creators to give up their copyright and put their works into the worldwide public domain. CC0 allows reusers to distribute, remix, adapt, and build upon the material in any medium or format, with no conditions.`
+  if (copyright === "Creative Commons - CC0"){
+    return `CC0 (aka CC Zero) is a public dedication tool, which allows creators to give up their copyright and put their works into the worldwide public domain. CC0 allows reusers to distribute, remix, adapt, and build upon the material in any medium or format, with no conditions.`
   }
-  if (badge === "Eclipse Public License (EPL)"){
-    return `[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)
-
-    THE ACCOMPANYING PROGRAM IS PROVIDED UNDER THE TERMS OF THIS ECLIPSE PUBLIC LICENSE ("AGREEMENT"). ANY USE, REPRODUCTION OR DISTRIBUTION OF THE PROGRAM CONSTITUTES RECIPIENT'S ACCEPTANCE OF THIS AGREEMENT.
+  if (copyright === "Eclipse Public License (EPL)"){
+    return `THE ACCOMPANYING PROGRAM IS PROVIDED UNDER THE TERMS OF THIS ECLIPSE PUBLIC LICENSE ("AGREEMENT"). ANY USE, REPRODUCTION OR DISTRIBUTION OF THE PROGRAM CONSTITUTES RECIPIENT'S ACCEPTANCE OF THIS AGREEMENT.
 
     For more information visit: https://www.eclipse.org/legal/epl-v10.html`
 
   }
 }
 
-const generateHTML = ({ title, description, installation, githubLink, deployedLink, userStory, acceptanceCriteria, contributing, email, github, tests, license, name }) =>
+const licenseBadge = (copyright) => {
+    if (copyright === "MIT License") {
+      return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+  }
+  if (copyright === "IBM Public License Version 1.0"){
+    return `[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)`
+  }
+  if (copyright === "The Apache License"){
+    return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+  }
+  if (copyright === "Creative Commons - CC0"){
+    return `[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)`
+  }
+  if (copyright === "Eclipse Public License (EPL)"){
+    return `[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)`
+  }
+
+}
+
+
+const generateHTML = ({ title, description, installation, contributing, email, github, tests, license, usage }) =>
   `# ${title}
 
 <a name="desc"></a>
 ## Description
 ${description}  
+
+${licenseBadge(license)}
+
 
 ## Table of Contents
 1. [ Description. ](#desc)
@@ -130,7 +144,7 @@ ${usage}
 
 <a name="license"></a>
 ## License
-  ${licenseBadge(license, name)}
+${copyrightText(license)}
 
   <a name="contrib"></a>
   # Contributing
@@ -158,81 +172,3 @@ const init = () => {
 };
 
 init();
-
-
-// // TODO: Include packages needed for this application
-// const inquirer = require('inquirer'); //require is part of node, requiring inquirer as seen in package.json, stored it in variable
-
-// // TODO: Create an array of questions for user input
-// const questions = [
-    
-// ];
-
-// // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
-
-// // TODO: Create a function to initialize app
-// function init() {
-//     inquirer
-//     .prompt([
-//       {
-//           type: "input",
-//           name: "Title",
-//           message: "What is the title of your project?"
-//       },
-//       {
-//           type: "input",
-//           name: "Description",
-//           message: "What is the description of your project?"
-//       },
-//       {
-//           type: "input",
-//           name: "Installation",
-//           message: "What are the installation instructions?"
-//       },
-//       {
-//           type: "input",
-//           name: "Usage",
-//           message: "What are the usage instructions?"
-//       },
-//       {
-//           type: "input",
-//           name: "Contributing",
-//           message: "Any Contributions?"
-//       },
-//       {
-//           type: "input",
-//           name: "Tests",
-//           message: "Any tests?"
-//       },
-//       {
-//           type: "input",
-//           name: "License", // i need to be a list of options
-//           choices: ["test1", "test2", "test3"]
-//       },
-//       {
-//           type: "input",
-//           name: "Username", //i turn into a link to the github account
-//           message: "What is your github username?"
-//       },
-//       {
-//           type: "input",
-//           name: "Email",
-//           message: "What is your email address?"
-//       }
-
-//     ])
-//     .then((answers) => {
-//       // Use user feedback for... whatever!!
-//       console.log(answers)
-//     })
-//     .catch((error) => {
-//       if (error.isTtyError) {
-//         // Prompt couldn't be rendered in the current environment
-//       } else {
-//         // Something else went wrong
-//       }
-//     });}
-
-// // Function call to initialize app
-// init();
